4b49d2a3b4ccbc882fd44f3d7e60caf76d3b04cd
tapa2 <tarastertuchnu@gmail.com>
2026-05-23 13:44:09 +0300

    feat: django project skeleton with apps

---
+++ config/urls.py
@@ -0,0 +1,23 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('projects/', include('projects.urls')),
+    path('tasks/', include('tasks.urls')),
+    path('ai/', include('ai_assistant.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
